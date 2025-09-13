d4702d11a5dc12344595b0728062841e5154524c
Luciano Santos <lucianodev.contato@gmail.com>
2025-09-12 22:12:13 -0300

    Order, Instant, ISO 8601

--- src/main/java/com/lucianodev/course/entities/User.java
+++ src/main/java/com/lucianodev/course/entities/User.java
@@ -5,6 +5,8 @@ import jakarta.persistence.*;
 
 import java.io.Serial;
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 
 @Entity
@@ -22,6 +24,9 @@ public class User  implements Serializable {
     private String email;
     private String phone;
     private String password;
+
+    @OneToMany(mappedBy = "client")
+    private List<Order> orders = new ArrayList<>();
 
     public User(){}
 
@@ -72,6 +77,10 @@ public class User  implements Serializable {
 
     public void setPassword(String password) {
         this.password = password;
+    }
+
+    public List<Order> getOrders() {
+        return orders;
     }
 
     @Override
