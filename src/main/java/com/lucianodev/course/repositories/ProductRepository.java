8c3090d620eb75fe3b57e5d7a63e59e27387d6b3
Luciano Santos <lucianodev.contato@gmail.com>
2025-09-15 09:59:18 -0300

    OrderItem, many-to-many association with extra atributes PART 1

--- src/main/java/com/lucianodev/course/repositories/ProductRepository.java
+++ src/main/java/com/lucianodev/course/repositories/ProductRepository.java
@@ -4,4 +4,6 @@ import com.lucianodev.course.entities.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
+
+
 }
