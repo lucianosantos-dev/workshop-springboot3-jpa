17776748eef8f4bc66c39d659e098d5baedb7905
Luciano Santos <lucianodev.contato@gmail.com>
2025-09-19 20:52:36 -0300

    Exception handling - update

--- src/main/java/com/lucianodev/course/services/UserService.java
+++ src/main/java/com/lucianodev/course/services/UserService.java
@@ -4,6 +4,7 @@ import com.lucianodev.course.entities.User;
 import com.lucianodev.course.repositories.UserRepository;
 import com.lucianodev.course.services.exceptions.DatabaseException;
 import com.lucianodev.course.services.exceptions.ResourceNotFoundException;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
@@ -43,9 +44,13 @@ public class UserService {
     }
 
     public User update(Long id, User obj) {
-        User entity = repository.getReferenceById(id);
-        updateData(entity, obj);
-        return repository.save(entity);
+        try {
+            User entity = repository.getReferenceById(id);
+            updateData(entity, obj);
+            return repository.save(entity);
+        } catch (EntityNotFoundException e) {
+            throw new ResourceNotFoundException(id);
+        }
     }
 
     private void updateData(User entity, User obj) {
